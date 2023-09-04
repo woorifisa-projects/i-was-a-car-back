054fd63eacad8238b5e27d9fe63ceb3067d85cca
Cheol-Cheol <dream174822@gmail.com>
2023-09-04 20:31:38 +0900

    refactor: PR 피드백 반영 [#2]

--- src/main/java/xyz/iwasacar/api/common/auth/email/service/EmailService.java
+++ src/main/java/xyz/iwasacar/api/common/auth/email/service/EmailService.java
@@ -1,13 +1,13 @@
 package xyz.iwasacar.api.common.auth.email.service;
 
-import java.util.Random;
+import java.util.UUID;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
@@ -18,7 +18,7 @@ import xyz.iwasacar.api.common.auth.email.EmailSession;
 import xyz.iwasacar.api.common.auth.email.exception.EmailServerException;
 
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
 public class EmailService {
 
@@ -28,7 +28,7 @@ public class EmailService {
 
 	private final EmailSession redisUtil;
 
-	public String sendMail(EmailMessage emailMessage) {
+	public String sendMail(final EmailMessage emailMessage) {
 
 		String authNum = createCode();
 
@@ -55,37 +55,20 @@ public class EmailService {
 
 	// 인증번호 생성 메서드
 	private String createCode() {
-		Random random = new Random();
-		StringBuffer key = new StringBuffer();
-
-		for (int i = 0; i < 5; i++) {
-			int index = random.nextInt(4);
-
-			switch (index) {
-				case 0:
-					key.append((char)((int)random.nextInt(26) + 97));
-					break;
-				case 1:
-					key.append((char)((int)random.nextInt(26) + 65));
-					break;
-				default:
-					key.append(random.nextInt(9));
-			}
-		}
-		return key.toString();
+		return UUID.randomUUID().toString().split("-")[0].substring(0, 6);
 	}
 
 	// thymeleaf를 통한 html 적용
-	public String setContext(String code) {
+	public String setContext(final String code) {
+
 		Context context = new Context();
 		context.setVariable("code", code);
 		return templateEngine.process("email", context);
 	}
 
-	public void confirmEmailCode(String email, String code) {
+	public void confirmEmailCode(final String email, final String code) {
 
 		redisUtil.updateEmailCode(email, code);
-
 	}
 
 }
