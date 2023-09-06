9e1fca335590bcaa21917e248bdc422a017aec52
Dongyeol <eastheat10@gmail.com>
2023-09-07 00:02:34 +0900

    refactor: 로깅 추가

--- src/main/java/xyz/iwasacar/api/domain/logs/aop/LoginLogAspect.java
+++ src/main/java/xyz/iwasacar/api/domain/logs/aop/LoginLogAspect.java
@@ -3,17 +3,15 @@ package xyz.iwasacar.api.domain.logs.aop;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import xyz.iwasacar.api.common.dto.response.CommonResponse;
+import xyz.iwasacar.api.common.auth.jwt.MemberClaim;
+import xyz.iwasacar.api.common.context.MemberClaimContext;
 import xyz.iwasacar.api.domain.logs.entity.Log;
 import xyz.iwasacar.api.domain.logs.repository.LogRepository;
-import xyz.iwasacar.api.domain.members.dto.response.MemberResponse;
 import xyz.iwasacar.api.domain.members.entity.Member;
-import xyz.iwasacar.api.domain.members.exception.MemberNotFoundException;
 import xyz.iwasacar.api.domain.members.repository.MemberRepository;
 
 @Aspect
@@ -29,15 +27,41 @@ public class LoginLogAspect {
 
 	}
 
+	@Pointcut("execution(* xyz.iwasacar.api.domain.members.controller.MemberController.logout(..))")
+	private void logoutMethod() {
+
+	}
+
+	@Pointcut("execution(* xyz.iwasacar.api.domain.members.controller.MemberController.updateMember(..))")
+	private void updateMethod() {
+
+	}
+
 	@Transactional
-	@AfterReturning(value = "loginMethod()", returning = "memberResponseResponseEntity")
-	public void publishLoginEvent(ResponseEntity<CommonResponse<MemberResponse>> memberResponseResponseEntity) {
-		MemberResponse memberResponse = memberResponseResponseEntity.getBody().getData();
+	@AfterReturning(value = "loginMethod()")
+	public void publishLoginEvent() {
 
-		Member member = memberRepository.findById(memberResponse.getId()).orElseThrow(MemberNotFoundException::new);
-		member.updateLastLogin();
+		logRepository.save(Log.login(getMember()));
+	}
 
-		logRepository.save(new Log(member));
+	@Transactional
+	@AfterReturning(value = "logoutMethod()")
+	public void publishLogoutEvent() {
+
+		logRepository.save(Log.logout(getMember()));
+	}
+
+	@Transactional
+	@AfterReturning(value = "updateMethod()")
+	public void publishUpdateEvent() {
+
+		logRepository.save(Log.update(getMember()));
+	}
+
+	private Member getMember() {
+
+		MemberClaim claim = MemberClaimContext.getClaim();
+		return memberRepository.getBy(claim.getMemberId());
 	}
 
 }
