268ed2129ebe673e7e5749e1a221246a1e7ea5bf
lc i m , m i n s u <68263815+MSlcim@users.noreply.github.com>
2023-09-07 10:56:26 +0900

    fix : 로그인 안되는 에러 해결 (#68)

--- src/main/java/xyz/iwasacar/api/domain/logs/aop/LoginLogAspect.java
+++ src/main/java/xyz/iwasacar/api/domain/logs/aop/LoginLogAspect.java
@@ -3,14 +3,17 @@ package xyz.iwasacar.api.domain.logs.aop;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import xyz.iwasacar.api.common.auth.jwt.MemberClaim;
 import xyz.iwasacar.api.common.context.MemberClaimContext;
+import xyz.iwasacar.api.common.dto.response.CommonResponse;
 import xyz.iwasacar.api.domain.logs.entity.Log;
 import xyz.iwasacar.api.domain.logs.repository.LogRepository;
+import xyz.iwasacar.api.domain.members.dto.response.MemberResponse;
 import xyz.iwasacar.api.domain.members.entity.Member;
 import xyz.iwasacar.api.domain.members.repository.MemberRepository;
 
@@ -38,10 +41,11 @@ public class LoginLogAspect {
 	}
 
 	@Transactional
-	@AfterReturning(value = "loginMethod()")
-	public void publishLoginEvent() {
-
-		logRepository.save(Log.login(getMember()));
+	@AfterReturning(value = "loginMethod()", returning = "response")
+	public void publishLoginEvent(ResponseEntity<CommonResponse<MemberResponse>> response) {
+		Long id = response.getBody().getData().getId();
+		Member member = memberRepository.getBy(id);
+		logRepository.save(Log.login(member));
 	}
 
 	@Transactional
