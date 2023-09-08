8c12e2239687d02e7e7f77d22f714df41691a7c8
Dongyeol <eastheat10@gmail.com>
2023-09-08 15:33:18 +0900

    feat: 회원 정보 조회 [#3] (#76)

--- src/main/java/xyz/iwasacar/api/common/config/WebMvcConfig.java
+++ src/main/java/xyz/iwasacar/api/common/config/WebMvcConfig.java
@@ -31,15 +31,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 
-		// 테스트용으로 /api/v1/members 추가했는데 나중에 커밋할때는 지워야한다 log도 지워야함
 		registry.addInterceptor(new BearerAuthInterceptor(parser, provider))
 			.addPathPatterns("/api/v1/**")
 			.excludePathPatterns("/api/v1/members/login", "/api/v1/members/signup", "/api/v1/products",
 				"/api/v1/products/[0-9]+", "/api/v1/auth/email",
-				"/api/v1/auth/email-confirm", "/api/v1/sales/**")
+				"/api/v1/auth/email-confirm", "/api/v1/sales/**",
+				"/api/v1/documents/**")
 			.order(1);
 
-		registry.addInterceptor(new AdminInterceptor(parser))
+		registry.addInterceptor(new AdminInterceptor())
 			.addPathPatterns("/api/v1/admin/**")
 			.order(2);
 	}
