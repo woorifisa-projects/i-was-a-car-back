be96e74a994dc1f3698430733cecaecb2ab2ffd4
Jo-ormi <sunjooos2@naver.com>
2023-09-12 08:37:35 +0900

    hotfix: 쿼리 테스트하던 코드 삭제

--- src/main/java/xyz/iwasacar/api/domain/resources/repository/ResourceRepositoryImpl.java
+++ src/main/java/xyz/iwasacar/api/domain/resources/repository/ResourceRepositoryImpl.java
@@ -84,62 +84,34 @@ public class ResourceRepositoryImpl implements ResourceRepositoryCustom {
 		 limit 10
 		 ;
 		 */
-		// return jpaQueryFactory
-		// 	.selectFrom(productImage)
-		// 	.join(productImage.product).fetchJoin()
-		// 	.join(productImage.resource).fetchJoin()
-		// 	.where(productImage.id.productId.eq(
-		// 			JPAExpressions
-		// 				.select(productImage.id.productId)
-		// 				.from(productImage)
-		// 				.where(productImage.product.status.ne(EntityStatus.DELETED)
-		// 					.and(productImage.product.id.eq(productImage.id.productId)))
-		// 				.groupBy(productImage.id.productId, productImage.id.resourceId)
-		// 				.orderBy(productImage.id.resourceId.asc())
-		// 				.limit(1))
-		// 		.and(littleThanLastProductId(lastProductId))
-		// 		.and(setCategory(category, keyword))
-		// 		.and(
-		// 			productImage.role.id.eq(
-		// 				JPAExpressions
-		// 					.select(role.id)
-		// 					.from(role)
-		// 					.where(role.name.eq(ADMIN))
-		// 					.limit(1)
-		// 			)
-		// 		)
-		// 	).orderBy(productImage.id.productId.desc(), productImage.id.resourceId.asc())
-		// 	.limit(10)
-		// 	.fetch();
-
-		/** select *
-		 from products_images pi
-		 join resources r on r.resource_no = pi.resource_no
-		 join products p on p.product_no = pi.product_no
-		 group by pi.product_no
-		 having max(pi.resource_no)
-		 order by pi.product_no desc
-		 limit 10;
-		 */
-
-		return jpaQueryFactory.selectFrom(productImage).join(productImage.product).fetchJoin()
+		return jpaQueryFactory
+			.selectFrom(productImage)
+			.join(productImage.product).fetchJoin()
 			.join(productImage.resource).fetchJoin()
-			.where(littleThanLastProductId(lastProductId)
-				.and(setCategory(category, keyword)).and(
+			.where(productImage.id.productId.eq(
+					JPAExpressions
+						.select(productImage.id.productId)
+						.from(productImage)
+						.where(productImage.product.status.ne(EntityStatus.DELETED)
+							.and(productImage.product.id.eq(productImage.id.productId)))
+						.groupBy(productImage.id.productId, productImage.id.resourceId)
+						.orderBy(productImage.id.resourceId.asc())
+						.limit(1))
+				.and(littleThanLastProductId(lastProductId))
+				.and(setCategory(category, keyword))
+				.and(
 					productImage.role.id.eq(
 						JPAExpressions
 							.select(role.id)
 							.from(role)
 							.where(role.name.eq(ADMIN))
 							.limit(1)
-					)))
-			.groupBy(productImage.id.productId)
-			.having(productImage.id.resourceId.eq(
-				JPAExpressions.select(productImage.id.resourceId.min())
-					.from(productImage)
-					.groupBy(productImage.id.productId)))
-			.orderBy(productImage.id.productId.desc())
-			.limit(10).fetch();
+					)
+				)
+			).orderBy(productImage.id.productId.desc(), productImage.id.resourceId.asc())
+			.limit(10)
+			.fetch();
+
 	}
 
 	@Override
@@ -148,7 +120,7 @@ public class ResourceRepositoryImpl implements ResourceRepositoryCustom {
 			.selectFrom(productImage)
 			.join(productImage.product).fetchJoin()
 			.join(productImage.resource).fetchJoin()
-			.where(productImage.product.id.eq(
+			.where(productImage.id.productId.eq(
 					JPAExpressions
 						.select(product.id)
 						.from(product)
@@ -213,8 +185,7 @@ public class ResourceRepositoryImpl implements ResourceRepositoryCustom {
 					JPAExpressions
 						.select(product.id)
 						.from(product)
-						.where(productImage.product.id.eq(product.id)
-							.and(product.status.ne(EntityStatus.DELETED)))
+						.where(productImage.id.productId.eq(product.id))
 						.limit(1)
 						.orderBy(productImage.id.resourceId.asc()))
 				.and(littleThanLastProductId(lastProductId))
